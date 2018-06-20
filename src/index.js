91a48e2a94233e290105efcba7839bf0ba8c5a09
JackVarney <jack.varney@iosh.com>
2018-06-20 23:33:26 +0100

    init commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+console.clear();
+
+const fs = require('fs');
+const path = require('path');
+const gm = require('gm').subClass({ imageMagick: true });
+const JSDOM = require('jsdom').JSDOM
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.get('/', (req, res) => {
+    fs.readFile(path.join(__dirname, '..', 'public', 'html', 'index.html'), (err, buffer) => {
+
+        const { document } = new JSDOM(buffer).window;
+        const imgTags = [...document.getElementsByTagName('img')];
+
+        Promise.all(
+            imgTags.map(el =>
+                new Promise(res => {
+                    const src = el.getAttribute('data-src');
+                    const split = src.split('/');
+                    const imgPath = path.join(__dirname, '..', 'public', 'images', split[split.length - 1]);
+                    const img = gm(imgPath).size((err, size) => {
+                        const { width, height } = size;
+
+                        img.resize(3, 3).toBuffer((err, buffer) => {
+                            const imgEl = document.querySelector(`[data-src="${src}"]`)
+                            imgEl.setAttribute('src', 'data:image/jpg;base64,' + buffer.toString('base64'));
+                            imgEl.setAttribute('height', height + 'px');
+                            imgEl.setAttribute('width', width + 'px');
+                            res();
+                        });
+                    });
+
+
+                })
+            )
+        ).then(() => {
+            res.send(document.documentElement.outerHTML);
+        });
+    });
+})
+
+app.listen(3005, () => console.log('listening'));
